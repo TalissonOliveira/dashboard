a86dde9d3a13615540a1947d62c7b56ebe7038ce
Talisson Oliveira <talissonlux@outlook.com>
2022-06-25 23:58:41 -0300

    feat: creates the login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,33 @@
+import { MdEmail, MdLock } from 'react-icons/md'
+import { AuthLayout } from '../../layouts/AuthLayout'
+import { Button } from '../../components/Button'
+
+import styles from './styles.module.scss'
+
 export function Login() {
   return (
-    <h1>Login</h1>
+    <AuthLayout>
+      <main className={styles.container}>
+        <div className={styles.content}>
+          <h1>Entrar</h1>
+          <p>Entre com suas credenciais.</p>
+
+          <form className={styles.form}>
+            <div className={styles.inputContainer}>
+              <input type="email" placeholder="E-mail" required />
+              <MdEmail size={24} />
+            </div>
+            <div className={styles.inputContainer}>
+              <input type="password" placeholder="Senha" required />
+              <MdLock size={24} />
+            </div>
+
+            <Button>Entrar</Button>
+          </form>
+        </div>
+
+        <p>Esqueceu a senha? <a href="#">Redefinir senha</a></p>
+      </main>
+    </AuthLayout>
   )
 }
