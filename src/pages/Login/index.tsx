2e06388552531c76dbe3c24c8a7dccb4af511f7b
Talisson Oliveira <talissonlux@outlook.com>
2022-06-26 21:16:50 -0300

    feat: adds error validation in login form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,36 @@
 import { MdEmail, MdLock } from 'react-icons/md'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { AuthLayout } from '../../layouts/AuthLayout'
 import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+import * as Yup from 'yup'
 
 import styles from './styles.module.scss'
 
+interface SignInFormData {
+  email: string
+  password: string
+}
+
+const signInFormSchema = Yup.object({
+  email: Yup.string().email('E-mail inválido').required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória')
+})
+
 export function Login() {
+  const { register, handleSubmit, formState: {
+    isSubmitting,
+    errors
+  }} = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 750))
+    console.log(values)
+  }
+
   return (
     <AuthLayout>
       <main className={styles.container}>
@@ -12,17 +38,31 @@ export function Login() {
           <h1>Entrar</h1>
           <p>Entre com suas credenciais.</p>
 
-          <form className={styles.form}>
-            <div className={styles.inputContainer}>
-              <input type="email" placeholder="E-mail" required />
-              <MdEmail size={24} />
-            </div>
-            <div className={styles.inputContainer}>
-              <input type="password" placeholder="Senha" required />
-              <MdLock size={24} />
-            </div>
+          <form onSubmit={handleSubmit(handleSignIn)} className={styles.form}>
+            <Input
+              type="email"
+              placeholder="E-mail"
+              required
+              error={errors.email}
+              icon={<MdEmail size={24} />}
+              {...register('email')}
+            />
 
-            <Button>Entrar</Button>
+            <Input
+              type="password"
+              placeholder="Senha"
+              required
+              error={errors.password}
+              icon={<MdLock size={24} />}
+              {...register('password')}
+            />
+
+            <Button
+              isLoading={isSubmitting}
+              disabled={isSubmitting}
+            >
+              Entrar
+            </Button>
           </form>
         </div>
 
